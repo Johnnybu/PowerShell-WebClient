cfacd19192a17c2784e3a888408405b57ff0da09
Johnnybu <github@johnnybu.com>
2015-02-18 09:43:04 -0500

    Init
    Initial addition of script files.

---
+++ XmlValidate.ps1
@@ -0,0 +1,31 @@
+﻿function Test-Xml {
+    param (
+    [Parameter(ValueFromPipeline=$true, Mandatory=$true)]
+        [string] $XmlFile,
+
+        [Parameter(Mandatory=$true)]
+        [string] $SchemaFile
+    )
+
+    [string[]]$Script:XmlValidationErrorLog = @()
+    [scriptblock] $ValidationEventHandler = {
+        $Script:XmlValidationErrorLog += $args[1].Exception.Message
+    }
+
+    $xml = New-Object System.Xml.XmlDocument
+    $schemaReader = New-Object System.Xml.XmlTextReader $SchemaFile
+    $schema = [System.Xml.Schema.XmlSchema]::Read($schemaReader, $ValidationEventHandler)
+    $xml.Schemas.Add($schema) | Out-Null
+    $xml.Load($XmlFile)
+    $xml.Validate($ValidationEventHandler)
+
+    if ($Script:XmlValidationErrorLog) {
+        Write-Warning "$($Script:XmlValidationErrorLog.Count) errors found"
+        Write-Error "$Script:XmlValidationErrorLog"
+        Exit
+    }
+    else {
+        Write-Log "The script is valid"
+        Return $xml
+    }
+}
